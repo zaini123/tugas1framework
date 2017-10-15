7fabb1d95e05ea4e42f8f72d389604c21b496c81
zainikrp <32807243+zaini123@users.noreply.github.com>
2017-10-15 13:43:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>COBA GRID</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-sm-4 col-1">
+<h4>KOLOM 1</h4>
+	</div>
+<div class="col-sm-4 col-2">
+<h4>KOLOM 2</h4>
+	</div>
+<div class="col-sm-4 col-3">
+<h4>KOLOM 3</h4>
+	</div>
+<div class="col-sm-4 col-4">
+<h4>Baris 1</h4>
+	</div>
+<div class="col-sm-4 col-5">
+<h4>Baris 1</h4>
+	</div>
+<div class="col-sm-4 col-6">
+<h4>Baris 1</h4>
+	</div>
+
+	
+</div>
+</div>
+<style> 
+.col-1{
+	background-color: #B80AB0;
+}
+.col-2{
+	background-color: #2ecc71;
+}
+.col-3{
+	background-color: #e74c3c;
+}
+.col-4{
+	background-color: #9900FF;
+}
+.col-5{
+	background-color: #660000;
+}
+.col-6{
+	background-color: #0099FF;
+}
+</style>
+</body>
+</html>
